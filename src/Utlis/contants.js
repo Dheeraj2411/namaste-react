d8125599e20f864fd4c1ff3026f7a9da425dc4e6
Dheeraj2411 <121419166+Dheeraj2411@users.noreply.github.com>
2024-01-05 01:58:28 +0530

    Update contants.js

--- src/Utlis/contants.js
+++ src/Utlis/contants.js
@@ -3,10 +3,10 @@ export const CDN_URL= "https://media-assets.swiggy.com/swiggy/image/upload/fl_lo
 export const LOGO_URL="https://imgs.search.brave.com/uOwUtQ-Fn7xcJBQXBJiIDVB-5uCgCaafBtfk9aJ7jg0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9keW5h/bWljLmJyYW5kY3Jv/d2QuY29tL2Fzc2V0/L2xvZ28vNTM3MDM2/NjMtZjJlYS00ZTgx/LThiODktNDQ5NjVi/MDczODAwL2xvZ28t/c2VhcmNoLWdyaWQt/MXg_bG9nb1RlbXBs/YXRlVmVyc2lvbj0x/JnY9NjM3NzgxNzY3/ODUzMDAwMDAw"
 
 export const restaurantsMenu_APi =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.453109&lng=77.037495&restaurantId=";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.453109&lng=77.037495&restaurantId=";
 
 export const resData_API =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.453109&lng=77.037495&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.453109&lng=77.037495&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 // export const resData_API =
 //   "https://corsproxy.org/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.453109&lng=77.037495&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
